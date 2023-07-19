39afaf211c75237ef511f971277c82245fdbddd0
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-18 22:06:27 -0300

    add styles

--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import estilos from "./estilos";
 
 
 export default function Item({ nome, preco, descricao }) {
-    return <View style={estilos.informacao}>
+
+
+    return <>
+    <View style={estilos.informacao}>
     <Text style={estilos.nome}>{nome}</Text>
     <Text style={estilos.descricao}>{descricao}</Text>
     <Text style={estilos.preco}>{preco}</Text>
     
     
     </View>
+
+    <View style={estilos.carrinho}>
+        <View>
+            <View style={estilos.valor}>
+                <Text style={estilos.quantidade}>Quantidade:</Text>
+                <TextInput/>
+            </View> 
+            <View style={estilos.valor}>
+                <Text style={estilos.quantidade}>Preço:</Text>
+                <Text style={estilos.preco}>0</Text>
+            </View> 
+        </View>
+    </View>
+    </> 
 }
