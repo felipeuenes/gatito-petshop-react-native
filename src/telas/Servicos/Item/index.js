86820fd40fcd3c283484ff361560710cf9178e2c
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-18 21:52:48 -0300

    add estilos

---
+++ src/telas/Servicos/Item/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+import estilos from "./estilos";
+
+
+export default function Item({ nome, preco, descricao }) {
+    return <View style={estilos.informacao}>
+    <Text style={estilos.nome}>{nome}</Text>
+    <Text style={estilos.descricao}>{descricao}</Text>
+    <Text style={estilos.preco}>{preco}</Text>
+    
+    
+    </View>
+}
--- src/telas/Servicos/index.js
+++ src/telas/Servicos/index.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { SafeAreaView , StatusBar ,Text } from "react-native";
+import { SafeAreaView, StatusBar, Text, FlatList} from "react-native";
+import Item from "./Item";
+
+const servicos = [
+    {
+        id: 1,
+        nome: "Banho",
+        preco: 79.9,
+        descricao: "Não dê banho no seu gato, mas se precisar nós damos",
+    },
+    {
+        id: 2,
+        nome: "Vacina V4",
+        preco: 89.9,
+        descricao: "Uma dose da vacina V4. Seu gato precisa de duas",
+    },
+    {
+        id: 3,
+        nome: "Vacina Antirrábica",
+        preco: 99.9,
+        descricao: "Uma dose da vacina Antirrábica. Seu gato precisa de uma por ano",
+    }
+]
 
 
 export default function Servicos() {
-    return <SafeAreaView>
-            <StatusBar>
-                    <Text>Serviços! testeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
-            </StatusBar>
+    return <>
+    <SafeAreaView>
+            <StatusBar />
+                    <Text>Serviços!</Text>
+            <FlatList 
+            data={servicos} 
+            renderItem={({item}) => <Item {...item}/>}  
+            keyExtractor={({id}) => String(id)}
+            />
         </SafeAreaView>
+    </>
 };
 
