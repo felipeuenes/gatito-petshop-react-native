b6ea61978cc7eb1c529318a92f5d89c5778e94ed
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-20 12:02:12 -0300

    add

--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -10,11 +10,10 @@ export default function Item({ nome, preco, descricao }) {
 
     return <>
     <View style={estilos.informacao}>
-    <Text style={estilos.nome}>{nome}</Text>
-    <Text style={estilos.descricao}>{descricao}</Text>
-    <Text style={estilos.preco}>{preco}</Text>
-    
-    
+        <Text style={estilos.nome}>{nome}</Text>
+        <Text style={estilos.descricao}>{descricao}</Text>
+        <Text style={estilos.preco}>{preco}</Text>
+        
     </View>
 
     <View style={estilos.carrinho}>
